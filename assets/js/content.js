244ff5e563a07395eaed62db6007d85d39f3534f
Eric C Schoenborn <cultureondemand@gmail.com>
2019-11-05 00:04:42 -0800

    Add files via upload

--- assets/js/content.js
+++ assets/js/content.js
@@ -141,6 +141,25 @@ var slideData = [
   },
   
 
+  {
+    section: "And I was getting work all over the place",
+//////////////////////////     icon: "pcy.svg",
+    header: "",
+    bkgImage: {
+      small: "aldiaillos.jpg",
+      medium: "aldiaillos.jpg",
+      large: "aldiaillos.jpg"
+    },
+    body: `
+<strong>All that smoking weed and drawing was starting to pay off.</strong><br /> Not pigeonholing myself as just one thing, defined me.  
+     `
+  },
+  
+
+
+
+
+
 
   {
     section: "And then this day happened",
@@ -170,26 +189,148 @@ var slideData = [
    bkgImage: {
     //////////// wm: "assets/images/bkg/zubair.webm",
        mp: "assets/images/bkg/idc.mp4",
-      name:"3"
+      name:"1"
     },
     body: `
-<strong>Multimedia was the word we used</strong><br />Video, JavaScript, WebGL, WebRTC, Projection Mapping<br />36' x 12'<br />March 13-18, 2017<br />Borscht Film Festival<br /><br />Clip from transmedia documentary created with support from Borscht Film Festival. I wrote, directed and edited the documentary and filmed with a small crew of Miami filmmakers supporting me during a 1 week shoot on Loop Road in Big Cypress Swamp. Released as projection mapping installation at Borsch Film Festival 2017. 
+<strong>Multimedia was the word we used</strong><br />
     `
   },
   
 
 
   {
-    section: "The Groves Homestead. Brethren, MI",
+    section: "Specialty interactive was now my specialty",
 //////////////////////////     icon: "pcy.svg",
     header: "",
     bkgImage: {
-      small: "barnface.png",
-      medium: "barnface.png",
-      large: "barnface.png"
+      small: "91.jpg",
+      medium: "91.jpg",
+      large: "91.jpg"
     },
     body: `
-<strong>Augmented Americana</strong><br />JavaScript, WebGL, HTML5, WebRTC, Projection Mapping<br />96' x 52'<br />June 7, 2018<br />Brethren, MI<br /><br />Long exposure photo of solo, off-the-grid projection mapping of audio and video responsive web app mapped onto a backwoods barn in Northern Michigan.   
+<strong>Flash seems dumb now, but it was dope then. And it opened up a ton of doors for media artists.</strong><br />   
+     `
+  },
+  
+
+  {
+    section: "Art & Storytelling for Impact on the Interwebs",
+//////////////////////////     icon: "pcy.svg",
+    header: "",
+    bkgImage: {
+      small: "chickenbrawl.jpg",
+      medium: "chickenbrawl.jpg",
+      large: "chickenbrawl.jpg"
+    },
+    body: `
+<strong>Completely blurred the lines between art and day job.</strong><br />   
+     `
+  },
+  
+
+  {
+    section: "I kept building skills and making for fun too. Because I have to.",
+//////////////////////////     icon: "pcy.svg",
+      header: "",
+    video: "yes",
+   bkgImage: {
+    //////////// wm: "assets/images/bkg/zubair.webm",
+       mp: "assets/images/bkg/dcstroll2004.mp4",
+      name:"2"
+    },
+    body: `
+<strong>More multimedia</strong><br />Everything was totally political. 
+    `
+  },
+  
+ 
+  {
+    section: "Art with Impact ... for money.",
+//////////////////////////     icon: "pcy.svg",
+    header: "",
+    bkgImage: {
+      small: "132.jpg",
+      medium: "132.jpg",
+      large: "132.jpg"
+    },
+    body: `
+<strong>So much room to experiment</strong><br />
+     `
+  },
+  
+
+  {
+    section: "My style started to develop",
+//////////////////////////     icon: "pcy.svg",
+    header: "",
+    bkgImage: {
+      small: "aclusafeandfreeillos.jpg",
+      medium: "aclusafeandfreeillos.jpg",
+      large: "aclusafeandfreeillos.jpg"
+    },
+    body: `
+<strong>Aesthetic kept evolving, but I started to have a characteristic look </strong><br /> 
+     `
+  },
+  
+
+  {
+    section: "Was rolling at this point",
+//////////////////////////     icon: "pcy.svg",
+    header: "",
+    bkgImage: {
+      small: "torturefoiasearchcomp.jpg",
+      medium: "torturefoiasearchcomp.jpg",
+      large: "torturefoiasearchcomp.jpg"
+    },
+    body: `
+<strong>Started to be able to make anything, on the spot</strong><br />
+     `
+  },
+  
+
+
+  {
+    section: "All mediums were benefiting and evolving",
+//////////////////////////     icon: "pcy.svg",
+    header: "",
+    bkgImage: {
+      small: "redwoodsnight.jpg",
+      medium: "redwoodsnight.jpg",
+      large: "redwoodsnight.jpg"
+    },
+    body: `
+<strong>Long exposures became my shit</strong><br />
+     `
+  },
+  
+ {
+    section: "Internets #1",
+//////////////////////////     icon: "pcy.svg",
+    header: "",
+    bkgImage: {
+      small: "lolacademics.jpg",
+      medium: "lolacademics.jpg",
+      large: "lolacademics.jpg"
+    },
+    body: `
+<strong>This era cemented my outlook on making and sharing</strong><br />Internet freedom work, mixed with radical interests, mixed with heavy impact, mixed with opportunity. 
+     `
+  },
+  
+
+  
+ {
+    section: "I fully formed my storygathering style at this point as well",
+//////////////////////////     icon: "pcy.svg",
+    header: "",
+    bkgImage: {
+      small: "148.jpg",
+      medium: "148.jpg",
+      large: "148.jpg"
+    },
+    body: `
+<strong>Explore, gather, enjoy, don't spend money, don't drive on Interstates</strong><br /> 
      `
   },
   
@@ -197,21 +338,155 @@ var slideData = [
 
 
   {
-    section: "New World Center for the Arts. Miami Beach, FL",
+    section: "Tech was starting to evolve. It was so on.",
 //////////////////////////     icon: "pcy.svg",
     header: "",
     bkgImage: {
-      small: "borscht.jpeg",
-      medium: "borscht.jpeg",
-      large: "borscht.jpeg"
+      small: "tumblr_mrwzbinMMo1re5rxgo1_500.gif",
+      medium: "tumblr_mrwzbinMMo1re5rxgo1_500.gif",
+      large: "tumblr_mrwzbinMMo1re5rxgo1_500.gif"
     },
     body: `
-<strong>New World Architecture Mapping</strong><br />Videography, Projection Mapping<br />Architecture Mapping<br />March 13-18, 2017<br />Borscht Film Festival<br /><br />Architecture mapping of the interior of Frank Gehry designed New World Center building on Miami Beach, for takeover of the space by innovative Miami film festival. Led setup and positioning of 9 projectors throughout the interior of the space and mapped in MadMapper.    
+<strong>I'd biult my muscles, and tech was starting to offer new avenues.</strong><br />  
     `
   },
   
 
+  
 
+
+  {
+    section: "Miami Design District. Miami, FL",
+//////////////////     icon: "pcy.svg",
+    header: "",
+ //////////////////////////   video: "yes",
+    bkgImage: {
+      small: "phenomenalradnessartpark.jpg",
+      medium: "phenomenalradnessartpark.jpg",
+      large: "phenomenalradnessartpark.jpg"
+      
+    },
+    body: `
+ <strong>Phenomenal Radness</strong><br />Processing, TUIO, MadMapper, Videography<br />120'x18'<br />June 2013<br />Miami Design District<br /><br />Created in partnership with NikeSB and MIA Skateshop as a popup immersive experience in the Miami Design District. I developed the piece in Processing and incorporated Microsoft Kinects using TUIO for a motion-reactive component. The piece was mapped across the space using MadMapper into 6 projectors placed and hung in collaboration with Ed Selego.
+    `
+  },
+  
+  
+  
+
+  
+
+
+  {
+    section: "Art Basel. Miami Beach, FL",
+//////////////////     icon: "pcy.svg",
+    header: "",
+ //////////////////////////   video: "yes",
+    bkgImage: {
+      small: "6-IMG_0284_new-1060x707.jpg",
+      medium: "6-IMG_0284_new-1060x707.jpg",
+      large: "6-IMG_0284_new-1060x707.jpg"
+      
+    },
+    body: `
+ <strong>Phenomenal Amarante</strong><br />Curation, Design, Development, JavaScript, WebGL<br />60'x20'<br />December 10, 2014<br />FIU-Wolfsonian Museum<br /><br />Sound Reactive projection mapping musical collaboration for official opening of Art Basel 2014. Organized the show, developed the browser app and executed the projector setup and mapping in collaboration with Ed Selego.
+    `
+  },
+  
+  
+  
+  
+  
+
+
+  {
+    section: "The Bacardi Building. Miami, FL",
+//////////////////     icon: "pcy.svg",
+    header: "",
+ //////////////////////////   video: "yes",
+    bkgImage: {
+      small: "youngarts.jpg",
+      medium: "youngarts.jpg",
+      large: "youngarts.jpg"
+      
+    },
+    body: `
+ <strong>Outside the Box: Jason Moran, Jazz & Skateboarding</strong><br />JavaScript, WebGL, Projection Mapping<br />36' x 24'<br />Feb 6, 2016<br />YoungArts Campus<br /><br />Created a sound reactive projection mapping installation inspired by the Spike Jonze/Mark Gonzalez masterpiece, Video Days. Built a 3D version of the video in JavaScript and HTML5 to be projected on a building across the plaza, designed to respond to live Jazz performed by Jason Moran as he and his band improvised to skateboarders skating a halfpipe in the plaza of Miami's YoungArts campus.
+    `
+  },
+  
+ 
+   
+  
+  
+  
+
+
+  {
+    section: "knightfoundation.org/features/storytellingexpeditions/",
+//////////////////     icon: "pcy.svg",
+    header: "",
+ //////////////////////////   video: "yes",
+    bkgImage: {
+      small: "interactiveexped.gif",
+      medium: "interactiveexped.gif",
+      large: "interactiveexped.gif"
+      
+    },
+    body: `
+ <strong>Interactive Storytelling Expeditions</strong><br />Creative Direction, Art Direction, Concepting, Writing, Design, Illustration, JavaScript, HTML5, Audio<br />December 1, 2014<br />Web Based Project<br /><br />A Knight Foundation original report I created to demonstrate one approach to immersive storytelling for organizations and publications.
+    `
+  },
+  
+  
+  
+  
+
+
+  {
+    section: "searchlightsandsunglasses.org",
+//////////////////     icon: "pcy.svg",
+    header: "",
+ //////////////////////////   video: "yes",
+    bkgImage: {
+      small: "searchlightsandsunglasses.png",
+      medium: "searchlightsandsunglasses.png",
+      large: "searchlightsandsunglasses.png"
+      
+    },
+    body: `
+ <strong>Searchlights and Sunglasses</strong><br />Creative Direction, Art Direction, Concepting<br />October 2013<br />Web Based Project<br /><br />Unique HTML5 journalism learning tool created in collaboration with author and journalism innovator Eric Newton.
+    `
+  },
+  
+  
+  
+
+
+  
+  
+    {
+    section: "Deauville Beach Resort. Miami Beach, FL",
+//////////////////////////     icon: "pcy.svg",
+    header: "",
+    video: "yes",
+   bkgImage: {
+     wm: "assets/images/bkg/mygoodgood.webm",
+       mp: "assets/images/bkg/mygoodgood.mp4",
+      name:"7"
+    },
+    body: `
+ <strong>My Good, Good Friend App</strong><br />Videography, Photography, Field Audio, JavaScript, WebGL, HTML5, WebVR<br />February 26, 2016 — February 27, 2016<br />Filmgate Interactive Creative Festival<br /><br />Sound-reactive 3D storyword and mobile VR experience designed for Miami transmedia festival, Filmgate.
+`
+  },
+  
+
+
+   
+  
+  
+  
+  
 
 
   {
@@ -257,254 +532,16 @@ header: "",
 ,
   
 
-
-
-  {
-    section: "Leonard Pearstein Gallery. Philadelphia, PA",
-//////////////////     icon: "pcy.svg",
-    header: "",
- //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "skate_1.jpg",
-      medium: "skate_1.jpg",
-      large: "skate_1.jpg"
-      
-    },
-    body: `
- <strong>Philly Radness</strong><br />Skatepark Design, JavaScript, WebGL, HTML5, WebRTC, Projection Mapping<br />96' x 12'<br />March 1 - May 1, 2016<br />Drexel University<br /><br />Adapted an interactive skatepark concept for a Philadelphia specific show around the closing of iconic skate spot, Love Park. Designed the park, mounted 6 projectors, hung 2 cameras with mics and built the setup in collaboration with Ed Selego. Designed and developed the sound and motion reactive browser app in JavaScript, WebRTC and HTML5.
-    `
-  },
-  
-  
-  
-
-  {
-    section: "Winter Music Conference. Miami, FL",
-header: "",
-    video: "yes",
-   bkgImage: {
-     wm: "assets/images/bkg/fordistas.webm",
-       mp: "assets/images/bkg/fordistas.mp4",
-      name:"6"
-    },
-
-    body:`
- <strong>Fordistas: Infiltrate 17.0</strong><br />JavaScript, WebGL, Projection Mapping, VR<br />Web Based Project<br />March 13, 2016<br />Product/81<br /><br />Sound-reactive projection and mobile VR experience designed for this long-running Miami Music Conference alternative event.
-
- `
-
-  }
-
-,
-  
-
-
-  {
-    section: "The Bacardi Building. Miami, FL",
-//////////////////     icon: "pcy.svg",
-    header: "",
- //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "youngarts.jpg",
-      medium: "youngarts.jpg",
-      large: "youngarts.jpg"
-      
-    },
-    body: `
- <strong>Outside the Box: Jason Moran, Jazz & Skateboarding</strong><br />JavaScript, WebGL, Projection Mapping<br />36' x 24'<br />Feb 6, 2016<br />YoungArts Campus<br /><br />Created a sound reactive projection mapping installation inspired by the Spike Jonze/Mark Gonzalez masterpiece, Video Days. Built a 3D version of the video in JavaScript and HTML5 to be projected on a building across the plaza, designed to respond to live Jazz performed by Jason Moran as he and his band improvised to skateboarders skating a halfpipe in the plaza of Miami's YoungArts campus.
-    `
-  },
-  
  
  
   
   
   
-    {
-    section: "Deauville Beach Resort. Miami Beach, FL",
-//////////////////////////     icon: "pcy.svg",
-    header: "",
-    video: "yes",
-   bkgImage: {
-     wm: "assets/images/bkg/mygoodgood.webm",
-       mp: "assets/images/bkg/mygoodgood.mp4",
-      name:"7"
-    },
-    body: `
- <strong>My Good, Good Friend App</strong><br />Videography, Photography, Field Audio, JavaScript, WebGL, HTML5, WebVR<br />February 26, 2016 — February 27, 2016<br />Filmgate Interactive Creative Festival<br /><br />Sound-reactive 3D storyword and mobile VR experience designed for Miami transmedia festival, Filmgate.
-`
-  },
-  
-
-
-  
-  
-
-
-  {
-    section: "InterContinental Hotel. Miami, FL",
-//////////////////////////     icon: "pcy.svg",
-    header: "",
-  //////////////  video: "yes",
-    bkgImage: {
-      small: "omiamiphenomenal2.gif",
-      medium: "omiamiphenomenal2.gif",
-      large: "omiamiphenomenal2.gif"
-      
-    },
-    body: `
- <strong>InterContinental Miami Art Showcase</strong><br />Curation, Design, Development, JavaScript, WebGL, Video, Photography<br />18-story LEDs, hotel marquee, 12 interior screens<br />November 2015 - January 2016<br />InterContinental Hotel Miami<br /><br />Curated a year long series with 4 Miami New Media artists, including myself, to take over the interior and exterior of the downtown Miami property with site specific art.
-`
-  },
-  
-
-
-  
-  
-  
   
   
   
 
-
-  {
-    section: "Art Basel. Miami Beach, FL",
-//////////////////     icon: "pcy.svg",
-    header: "",
- //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "6-IMG_0284_new-1060x707.jpg",
-      medium: "6-IMG_0284_new-1060x707.jpg",
-      large: "6-IMG_0284_new-1060x707.jpg"
-      
-    },
-    body: `
- <strong>Phenomenal Amarante</strong><br />Curation, Design, Development, JavaScript, WebGL<br />60'x20'<br />December 10, 2014<br />FIU-Wolfsonian Museum<br /><br />Sound Reactive projection mapping musical collaboration for official opening of Art Basel 2014. Organized the show, developed the browser app and executed the projector setup and mapping in collaboration with Ed Selego.
-    `
-  },
-  
-  
-  
-  
-  
-  
-  
-  
-
-
-  {
-    section: "Patricia and Phillip Frost Museum of Science. Miami, FL",
-//////////////////     icon: "pcy.svg",
-    header: "",
- //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "scimuseum.jpg",
-      medium: "scimuseum.jpg",
-      large: "scimuseum.jpg"
-      
-    },
-    body: `
- <strong>Next – From Nano to Macro, Innovation at Every Scale</strong><br />JavaScript, WebGL<br />60'x20'<br />October 14, 2014<br />Patricia and Phillip Frost Museum of Science<br /><br />A real-life animated GIF visitors could step inside co-create together. The immersive, interactive experience was brought to life by the creative technology of projection mapping, motion sensing and beautiful content. Organized the show, developed the browser app and executed the projector setup and mapping in collaboration with Ed Selego.
-    `
-  },
-  
-  
-  
-  
-  
-  
-
-
-  {
-    section: "knightfoundation.org/features/storytellingexpeditions/",
-//////////////////     icon: "pcy.svg",
-    header: "",
- //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "interactiveexped.gif",
-      medium: "interactiveexped.gif",
-      large: "interactiveexped.gif"
-      
-    },
-    body: `
- <strong>Interactive Storytelling Expeditions</strong><br />Creative Direction, Art Direction, Concepting, Writing, Design, Illustration, JavaScript, HTML5, Audio<br />December 1, 2014<br />Web Based Project<br /><br />A Knight Foundation original report I created to demonstrate one approach to immersive storytelling for organizations and publications.
-    `
-  },
-  
-  
-  
-  
-
-
-  {
-    section: "searchlightsandsunglasses.org",
-//////////////////     icon: "pcy.svg",
-    header: "",
- //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "searchlightsandsunglasses.png",
-      medium: "searchlightsandsunglasses.png",
-      large: "searchlightsandsunglasses.png"
-      
-    },
-    body: `
- <strong>Searchlights and Sunglasses</strong><br />Creative Direction, Art Direction, Concepting<br />October 2013<br />Web Based Project<br /><br />Unique HTML5 journalism learning tool created in collaboration with author and journalism innovator Eric Newton.
-    `
-  },
-  
-  
-  
-  
-
-
-  {
-    section: "Miami Design District. Miami, FL",
-//////////////////     icon: "pcy.svg",
-    header: "",
- //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "phenomenalradnessartpark.jpg",
-      medium: "phenomenalradnessartpark.jpg",
-      large: "phenomenalradnessartpark.jpg"
-      
-    },
-    body: `
- <strong>Phenomenal Radness</strong><br />Processing, TUIO, MadMapper, Videography<br />120'x18'<br />June 2013<br />Miami Design District<br /><br />Created in partnership with NikeSB and MIA Skateshop as a popup immersive experience in the Miami Design District. I developed the piece in Processing and incorporated Microsoft Kinects using TUIO for a motion-reactive component. The piece was mapped across the space using MadMapper into 6 projectors placed and hung in collaboration with Ed Selego.
-    `
-  },
-  
-  
-  
-  
-  
-  
-  
-  
-
-
-  {
-    section: "Bass Museum. Miami Beach, FL",
-//////////////////     icon: "pcy.svg",
-    header: "",
- //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "eraseypagescreen.jpg",
-      medium: "eraseypagescreen.jpg",
-      large: "eraseypagescreen.jpg"
-      
-    },
-    body: `
- <strong>Erasey Page</strong><br />HTML5, JavaScript, Creative Direction, Design<br />June 2011<br />Bass Museum<br /><br />Erasey Page is a commissioned web-based project produced by Jillian Mayer in collaboration with creative technologist Eric Schoenborn, which will was on view in the project room space of the Bass Museum Miami Beach. 
-    `
-  },
-  
-  
-  
-  
-  
-  
-  
-
+ 
 
   {
     section: "www.ericcad.es",
